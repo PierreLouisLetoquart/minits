6d389eb0a453835b76937e428c576de2d7c99946
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2025-06-20 01:06:14 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { RangeError } from "./Errors";
 export { LinkedList } from "./LinkedList";
 export { DoublyLinkedList } from "./DoublyLinkedList";
+export { Queue } from "./Queue";
+export { Deque } from "./Deque";
